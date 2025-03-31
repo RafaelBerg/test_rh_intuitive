919f880a65bf17e7413222c22beb813d7e2d5537
Rafael Berg Medeiros Miranda <rafaelberg32@gmail.com>
2025-03-31 15:52:35 -0300

    Fix database connection issue by ensuring database creation before connecting

--- backend/database/config.py
+++ backend/database/config.py
@@ -79,13 +79,11 @@ def main():
         db_host = os.getenv("DB_HOST")
         db_user = os.getenv("DB_USER")
         db_password = os.getenv("DB_PASSWORD")
-        db_name = os.getenv("DB_NAME")
 
         conn = mysql.connector.connect(
             host=db_host,
             user=db_user,
             password=db_password,
-            database=db_name,
             allow_local_infile=True
         )
         cursor = conn.cursor()
